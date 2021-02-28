add3cf5213ab503e6447a4dc77e705b9450e6e38
Mukhtar Ahmad Osmani <mosmani@vibrenthealth.com>
2021-02-28 15:12:28 -0500

    first Commit

---
+++ src/main/java/com/maoptech/demo/SpringbootJenkinsDockerTestApplication.java
@@ -0,0 +1,13 @@
+package com.maoptech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsDockerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinsDockerTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maoptech/demo/SpringbootJenkinsDockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maoptech.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsDockerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
